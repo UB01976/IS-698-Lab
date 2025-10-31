e093708dfa395cc7b31f0f7238cda7746c852353
UB01976 <UB01976@umbc.edu>
2025-10-31 14:43:16 -0400

    Add files via upload

---
+++ Terraform-Homework2/modules/S3/S3-main.tf
@@ -0,0 +1,12 @@
+variable "bucket_name" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = "private"
+  tags   = { Name = var.bucket_name }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
+
