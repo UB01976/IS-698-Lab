7cfb48bb4f69fc62d28faeb3e0a2cae3f6f7dde3
UB01976 <UB01976@umbc.edu>
2025-10-31 14:45:36 -0400

    Add files via upload

---
+++ Terraform-Homework2/modules/dynamodb/dynamodb-main.tf
@@ -0,0 +1,25 @@
+variable "table_name" {}
+
+resource "aws_dynamodb_table" "user_logins" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserID"
+  range_key    = "Timestamp"
+
+  attribute {
+    name = "UserID"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "N"
+  }
+
+  tags = { Name = var.table_name }
+}
+
+output "table_name" {
+  value = aws_dynamodb_table.user_logins.name
+}
+
